aec4ddb751e138ff020e4f44b28d9e3471ec58a2
Yorian Peters <ypeters@kembit.nl>
2023-05-11 10:07:07 +0200

    Working (basic) application

---
+++ turving_volupia/lib/data/models/turv_model.dart
@@ -0,0 +1,32 @@
+class TurvCollection {
+  List<TurvableItem> items = [];
+
+  TurvCollection(this.items);
+
+  TurvCollection.exampleCollection() {
+    items = [
+      TurvableItem("Bier", 1),
+      TurvableItem("Wijn", 1.5),
+      TurvableItem("Fris", 1),
+    ];
+  }
+}
+
+class TurvableItem {
+  String name;
+  double cost;
+  int count = 0;
+
+  TurvableItem(this.name, this.cost);
+
+  void add(int amount) {
+    count += amount;
+    print("Added $amount to $name: $count");
+  }
+
+  void remove(int amount) {
+    count -= amount;
+    if (count < 0) count = 0;
+    print("Removed $amount from $name: $count");
+  }
+}
--- turving_volupia/lib/screens/home_screen.dart
+++ turving_volupia/lib/screens/home_screen.dart
@@ -1,16 +1,84 @@
 import 'package:flutter/material.dart';
 
+import '../data/models/turv_model.dart';
 import '../shared/menu_drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TurvCollection collection = TurvCollection.exampleCollection();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home")),
       drawer: const MenuDrawer(),
-      body: const Placeholder(),
+      body: ListView.builder(
+        itemCount: collection.items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return turvItemCard(collection.items[index]);
+        },
+      ),
+    );
+  }
+
+  Widget turvItemCard(TurvableItem item) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        color: Colors.redAccent,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  item.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        item.remove(1);
+                      });
+                    },
+                    icon: const Icon(Icons.exposure_neg_1),
+                  ),
+                  Text(
+                    item.count.toString(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        item.add(1);
+                      });
+                    },
+                    icon: const Icon(Icons.exposure_plus_1),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
